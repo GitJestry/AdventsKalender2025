4f491c22af96f88706dcfc4e334629a9b91278ba
Julian Meyer <jestry.privat@outlook.de>
2025-11-29 04:33:50 +0100

    quick debug

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const ADVENT_CONFIG = {
   recipientName: "meine Celine",
   missionIntro:
     "Bevor du deine echte Adventstür öffnen darfst, musst du die tägliche Challenge bestehen ♥",
-  debugMode: false,
+  debugMode: true,
   year: null,
 
   // Zentrale Spiele-Definitionen: hier neue Spiele eintragen.
